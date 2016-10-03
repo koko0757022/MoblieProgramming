1d35a4bcc64f8f64ddacc24d17a3c18045be94c3
koko0757022 <koko0757022@naver.com>
2016-10-03 13:59:34 +0900

    homework
    homework

--- QuickCoding01/app/src/main/java/kr/soen/quickcoding01/QuickCoding01.java
+++ QuickCoding01/app/src/main/java/kr/soen/quickcoding01/QuickCoding01.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuickCoding01 extends AppCompatActivity {
@@ -17,17 +18,25 @@ public class QuickCoding01 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_coding01);
 
+
+
         Button avg = (Button) findViewById(R.id.avg);
         Button min = (Button) findViewById(R.id.min);
 
+
         avg.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-            String s = Integer.toString(a.getRsult(a.AvgResult(num)));
+
+                TextView re = (TextView)findViewById(R.id.result);
+                re.setText("평균 = "+a.getRsult(a.AvgResult(num)));
+                String s = Integer.toString(a.getRsult(a.AvgResult(num)));
                 Toast.makeText(QuickCoding01.this, s, Toast.LENGTH_SHORT).show();
             }
         });
         min.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
+                TextView re = (TextView)findViewById(R.id.result);
+                re.setText("평균 = "+a.getRsult(m.MinResult(num)));
                 String s1 = Integer.toString(a.getRsult(m.MinResult(num)));
                 Toast.makeText(QuickCoding01.this, s1, Toast.LENGTH_SHORT).show();
             }
